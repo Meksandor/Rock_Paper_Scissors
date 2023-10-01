8c0294e202bada8e4d2c93a3e2f2002f08678dec
Meksandor <526473+gb96@users.noreply.github.com>
2023-10-01 12:36:58 +0300

    Complete playRound function body
    Complete playRound function body to decide according to the selected values of playSelection and computerSelection.

--- script.js
+++ script.js
@@ -10,8 +10,22 @@ function getComputerChoice(computerSelection) {
 
 }
 
-function playRound(playerSelection, computerSelection) {}
- 
+function playRound(playerSelection, computerSelection) {
+    switch (playerSelection, computerSelection) {
+        case (playerSelection === "Rock" && computerSelection === "Paper"):
+            console.log("You lost! Paper beats Rock");
+        case (playerSelection === "Rock" && computerSelection === "Scissors"):
+            console.log("You win! Rock beats Scissors");
+        case (playerSelection === "Paper" && computerSelection === "Scissors"):
+            console.log("You lost! Scissors beats Paper");
+        case (playerSelection === "Paper" && computerSelection === "Rock"):
+            console.log("You win! Paper beats Rock");
+        case (playerSelection === "Scissors" && computerSelection === "Rock"):
+            console.log("You lost! Rock beats Scissors");
+        case (playerSelection === "Scissors" && computerSelection === "Paper"):
+            console.log("You win! Scissors beats Paper");
+    }
+}
 
 function game() {
 
