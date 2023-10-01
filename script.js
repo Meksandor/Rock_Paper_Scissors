8af74e2f8666c80afe401b5316b1b3e1b91d44cc
Meksandor <526473+gb96@users.noreply.github.com>
2023-10-01 12:35:21 +0300

    Create basic JavaScript function skeleton
    Created brief JavaScript required function tree according to the instructions of Rock-Paper-Scissors

---
+++ script.js
@@ -0,0 +1,18 @@
+var Rock;
+var Paper;
+var Scissors;
+var playerSelectionAnyWord = prompt("Choose your weapon! :", "")
+var playerSelection
+
+
+
+function getComputerChoice(computerSelection) {
+
+}
+
+function playRound(playerSelection, computerSelection) {}
+ 
+
+function game() {
+
+}
