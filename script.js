d27843bac18c24004cc3512c3cde164c2e931e7e
Meksandor <526473+gb96@users.noreply.github.com>
2023-10-02 11:21:52 +0300

    Complete function playRound and test the game
    playRound function is built and game tested for one round playability with printing results to the console.

--- script.js
+++ script.js
@@ -28,11 +28,32 @@ function getComputerChoice() {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    
+function playRound() {
+    if (playerSelection === "Rock" && computerSelection === "Paper") {
+        console.log("Computer selection is :" + " " + computerSelection);
+        console.log("You lost! Paper beats Rock");
+    }
+    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        console.log("Computer selection is :" + " " + computerSelection);
+        console.log("You win! Rock beats Scissors");
+    }
+    else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        console.log("Computer selection is :" + " " + computerSelection);
+        console.log("You lost! Scissors beats Paper");
+    }
+    else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        console.log("Computer selection is :" + " " + computerSelection);
+        console.log("You win! Paper beats Rock");
+    }
+    else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        console.log("Computer selection is :" + " " + computerSelection);
+        console.log("You lost! Rock beats Scissors");
+    }
+    else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        console.log("Computer selection is :" + " " + computerSelection);
+        console.log("You win! Scissors beats Paper");
+    }
 }
-
-
 
 function game() {
 
